80b144c7f24c14887913305dd6456a9fedc6252b
Tobias A. Rau <github@tar-it.com>
2023-10-06 14:45:22 +0200

    Install python-decouple/Decouple security key

--- lyfetree/settings.py
+++ lyfetree/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tor2g31cydni0a4kb_$ex#z$-p=kvi_z#ua7ucyj@vits0u*v+"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
